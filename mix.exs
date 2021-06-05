bc2a89ed2f3cf42060c5d84c2784e8cebe7b95ca
Dimitris Zorbas <dimitrisplusplus@gmail.com>
2021-06-05 12:57:14 +0100

    Release version 0.1.1

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Opus.Graph.MixProject do
   def project do
     [
       app: :opus_graph,
-      version: "0.1.0",
+      version: "0.1.1",
       elixir: "~> 1.4",
       elixirc_paths: elixirc_paths(Mix.env()),
       build_embedded: Mix.env() == :prod,
